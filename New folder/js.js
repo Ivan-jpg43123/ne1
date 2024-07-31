4134a60ee1e1763618a0dba15aee38ef52e79d70
Ivan-jpg43123 <ivan.sedoriosa@gmail.com>
2024-07-31 22:01:57 +0800

    Add files via upload

---
+++ New folder/js.js
@@ -0,0 +1,18 @@
+function changeImage()
+{
+
+var img = document.getElementById("image1");
+img.src = 'nigur.jpg';
+
+   
+  
+}
+
+
+document.getElementById('button1').addEventListener('click', function() {
+    var audio = document.getElementById('audio1');
+    audio.play();
+});
+
+    
+    
